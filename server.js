1c169e7e10d4ee712fbe037e74d9f0fefdb46a4c
Rahul Vashishth <vrahul1991@gmail.com>
2017-01-23 09:52:36 +0530

    added morgan for dev logging

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const expressApp = express();
 const mongoose = require('mongoose');
 const uriUtil = require('mongodb-uri');
+const morgan = require('morgan');
 
 const hostName = 'localhost';
 const port = process.env.PORT || 3002;
@@ -14,10 +15,12 @@ const mongooseUri = uriUtil.formatMongoose(mongodbUri);
 const dbOptions = {};
 
 expressApp.use(bodyParser.json());
-expressApp.use(bodyParser.urlencoded({extended:true}));
+expressApp.use(bodyParser.urlencoded({extended:false}));
 expressApp.use(cors());
+expressApp.use(morgan('dev'));
+// add morgan middleware here 
 
-expressApp.use('/api/contacts',require('./api/routes/driver'));
+expressApp.use('/api/driver',require('./api/routes/driver'));
 
 // first value must be port, then host name and then callback function
 expressApp.listen(port, ()=>{
